065eb804955c5a50b33313eee4b71730b7193e5c
jet_black_ninja <pramodkmr958@gmail.com>
2024-08-13 13:40:45 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { useReactToPrint } from "react-to-print";
+import "./style.css";
+
+function App() {
+  const [] = useState()
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
